2bc0ad296ecb5204644d79781bffc05ecfae9b89
gatpanramadhan <ghatfhanamadhan21@gmail.com>
2024-03-20 20:12:52 +0700

    contoh1.php

---
+++ contoh1.php
@@ -0,0 +1,12 @@
+<?php
+class Contoh1 extends CI_Controller
+{
+ public function index()
+ {
+ echo "<h1>Ghatfhan Ramadhan</h1>";
+ echo"Nama saya Ghatfhan Ramadhan
+ Saya tingga di daerah tambun selatan
+ olah raga yang saya sukai adalah
+ Bulutangkis";
+ }
+}
